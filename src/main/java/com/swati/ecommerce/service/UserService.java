0736773657bfee2f622d01d26a223416722d79b7
swatinagare <swati.nagare412@gmail.com>
2023-06-25 16:20:45 +0530

    This is the fourth commit..

--- src/main/java/com/swati/ecommerce/service/UserService.java
+++ src/main/java/com/swati/ecommerce/service/UserService.java
@@ -1,13 +1,15 @@
 package com.swati.ecommerce.service;
 
 import com.swati.ecommerce.dao.UserDto;
+import com.swati.ecommerce.payloads.PageableResponse;
+
 import java.util.List;
 
 public interface UserService {
 
     UserDto createUser(UserDto userDto);
     UserDto updateUser(UserDto userDto, String userId);
-    List<UserDto> getAllUser();
+    PageableResponse<UserDto> getAllUser(Integer pageNumber, Integer pageSize, String sortBy, String sortDir);
     UserDto getUserById(String userId);
     UserDto getUserByEmail(String userEmail);
     void deleteUser(String userId);
