275c30e470792c02b9d5d137da7457a04a0be630
swatinagare <swati.nagare412@gmail.com>
2023-06-25 16:20:34 +0530

    This is the third commit..

--- src/main/java/com/swati/ecommerce/dao/UserDto.java
+++ src/main/java/com/swati/ecommerce/dao/UserDto.java
@@ -2,6 +2,7 @@ package com.swati.ecommerce.dao;
 
 import com.swati.ecommerce.validate.ImageNameValid;
 import lombok.*;
+
 import javax.validation.constraints.*;
 
 @Getter
@@ -13,11 +14,11 @@ public class UserDto {
     private String userId;
 
     @NotBlank
-    @Size(min = 4,max = 15,message = "Plz enter Valid UserName")
+    @Size(min = 4, max = 15, message = "Plz enter Valid UserName Min=4 or Max=15")
     private String name;
 
     //@Email(message = "Email Is not according to standards..")
-    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([a-z0-9]+\\.)+[a-z]{2,5}$",message = "Invalid User Email..!!")
+    @Pattern(regexp = "^[a-z0-9][-a-z0-9._]+@([a-z0-9]+\\.)+[a-z]{2,5}$", message = "Invalid User Email..!!")
     @NotBlank(message = "Email is Required")
     private String email;
 
@@ -28,7 +29,7 @@ public class UserDto {
     @NotBlank(message = "Specify gender!!")
     private String gender;
 
-    @Size(min = 10,max = 1000)
+    @Size(min = 10, max = 1000)
     @NotBlank(message = "Write something about Yourself!!")
     private String about;
 
