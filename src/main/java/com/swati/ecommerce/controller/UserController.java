ec36e801a01e64770f7305a9843a7fc607af93f3
swatinagare <swati.nagare412@gmail.com>
2023-06-25 16:32:33 +0530

    Added in Controller layer in User Module..

--- src/main/java/com/swati/ecommerce/controller/UserController.java
+++ src/main/java/com/swati/ecommerce/controller/UserController.java
@@ -3,15 +3,25 @@ package com.swati.ecommerce.controller;
 import com.swati.ecommerce.dao.UserDto;
 import com.swati.ecommerce.helper.AppConstant;
 import com.swati.ecommerce.payloads.ApiResponse;
+import com.swati.ecommerce.payloads.ImageResponse;
+import com.swati.ecommerce.payloads.PageableResponse;
+import com.swati.ecommerce.service.FileService;
 import com.swati.ecommerce.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -19,6 +29,12 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${user.profile.image.path}")
+    private String imageUploadPath;
     Logger logger = LoggerFactory.getLogger(UserController.class);
 
     /**
@@ -50,21 +66,25 @@ public class UserController {
     }
 
     /**
-     * @return
+     * @return List</ userDto>
      * @author Swati Nagare
      * @apiNote this is the api related to get All user
      */
     @GetMapping("/users")
-    public ResponseEntity<List<UserDto>> getAllUser() {
+    public ResponseEntity<PageableResponse<UserDto>> getAllUser(
+            @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstant.SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstant.SORT_DIR, required = false) String sortDir) {
         logger.info("Request Entering for get All  user");
-        List<UserDto> getAllUser = this.userService.getAllUser();
+        PageableResponse<UserDto> getAllUser = this.userService.getAllUser(pageNumber, pageSize, sortBy, sortDir);
         logger.info("Complete Request for get All  user ");
         return new ResponseEntity<>(getAllUser, HttpStatus.OK);
     }
 
     /**
      * @param userId
-     * @return
+     * @return userDto
      * @author Swati Nagare
      * @apiNote this is the api related to get user by userId
      */
@@ -78,7 +98,7 @@ public class UserController {
 
     /**
      * @param userEmail
-     * @return
+     * @return userDto
      * @author Swati Nagare
      * @apiNote this is the api related to get user by userEmail
      */
@@ -92,6 +112,7 @@ public class UserController {
 
     /**
      * @param userId
+     * @return ApiResponse
      * @author Swati Nagare
      * @apiNote this is the api related to delete user by userId
      */
@@ -99,8 +120,12 @@ public class UserController {
     public ResponseEntity<ApiResponse> deleteUser(@PathVariable String userId) {
         logger.info("Request Entering for delete single user with userId :{}", userId);
         this.userService.deleteUser(userId);
+        ApiResponse apiResponse=new ApiResponse();
+        apiResponse.setMessage(AppConstant.USER_DELETE);
+        apiResponse.setSuccess(true);
+        apiResponse.setStatus(HttpStatus.OK);
         logger.info("Complete Request for delete single user with userId :{}", userId);
-        return new ResponseEntity<ApiResponse>(new ApiResponse(AppConstant.USER_DELETE, true), HttpStatus.OK);
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
 
     /**
@@ -116,5 +141,46 @@ public class UserController {
         logger.info("Complete Request for search single user with keyword :{}", keyword);
         return new ResponseEntity<List<UserDto>>(list, HttpStatus.OK);
     }
+
+    /**
+     * @param image,userId
+     * @return
+     * @author Swati Nagare
+     * @apiNote this is the api related to uploadImage  by userId
+     */
+
+    @PostMapping("users/image/{userId}")
+    public ResponseEntity<ImageResponse> uploadImage(@RequestParam("userImage") MultipartFile image,
+                                                     @PathVariable String userId) throws IOException {
+
+        logger.info("Request Entering for upload image with User id: {} & saving image into DB", userId);
+        String imageName = fileService.uploadFile(image, imageUploadPath);
+        UserDto user = userService.getUserById(userId);
+        user.setImageName(imageName);
+        UserDto updatedUser = userService.updateUser(user, userId);
+        ImageResponse imageResponse = ImageResponse.builder().imageName(imageName).status(HttpStatus.CREATED).success(true).message(AppConstant.FILE_UPLOADED).build();
+        logger.info("Completed request for upload image with User id: {}", userId);
+        return new ResponseEntity<>(imageResponse, HttpStatus.CREATED);
+    }
+
+    /**
+     * @param userId
+     * @return image
+     * @author Swati Nagare
+     * @apiNote this is the api related to serveImage by userId
+     */
+    @GetMapping("/users/image/{userId}")
+    public void serveImage(@PathVariable String userId, HttpServletResponse response) throws IOException {
+        logger.info("Request Entering for download image with User id: {}", userId);
+        UserDto userDto = userService.getUserById(userId);
+        logger.info("User image name : {}", userDto.getImageName());
+        InputStream inputStream = fileService.getResource(imageUploadPath, userDto.getImageName());
+
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(inputStream, response.getOutputStream());  // StreamUtils copy data from inputStream to servlet response ie. it will write data to Servlet response
+        logger.info("Completed request for download image with User id: {}", userId);
+    }
+
+
 
 }
